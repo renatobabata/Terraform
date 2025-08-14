9cb12ff2a5a3befab806cb9283c831e3d8359003
Renato Babata <babatarenato@gmail.com>
2025-08-14 11:53:02 +0200

    Terraform update 14/08

---
+++ 01-terraform-basics/outputs.tf
@@ -0,0 +1,11 @@
+output "my_s3_bucket_versioning" {
+    value = aws_s3_bucket_versioning.versioning_example.versioning_configuration[0].status
+}
+
+output "my_s3_bucket_complete_details" {
+    value = aws_s3_bucket.my_s3_bucket
+}
+
+output "my_iam_user_complete_details" {
+    value = aws_iam_user.my_iam_user
+}
---
+++ 01-terraform-basics/resources.tf
@@ -0,0 +1,15 @@
+# resource "provider + resource type" "name of the object in Terraform" {}
+resource "aws_s3_bucket" "my_s3_bucket"{
+    bucket = "my-s3-bucket-renato-terraform-02"
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_updated"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+    bucket = aws_s3_bucket.my_s3_bucket.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+} 

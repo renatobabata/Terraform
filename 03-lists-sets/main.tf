37a6694282b8afdad815dc81103ef3afbaf0ef24
Renato Babata <babatarenato@gmail.com>
2025-08-15 12:35:19 +0200

    Terraform 16 list

---
+++ 03-lists-sets/main.tf
@@ -0,0 +1,27 @@
+variable "name" {
+    default = ["renato", "seiji", "terraform"]
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.8"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  count = length(var.name)
+  name  = var.name[count.index]
+}
+
+# 1 - plan -> before executing the command, we must test if the configuration is valid.
+# 2 - execute -> if the plan is successfull, then we can execute it.
+
+# STATE
+# DESIRED - KNOWN - ACTUAL
